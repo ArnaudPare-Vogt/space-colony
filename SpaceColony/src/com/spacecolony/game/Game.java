7497d456a731b46b0217bcd11306fdb77bd71e7e
1448607 <1448607@li-h023-0t02>
2015-12-11 10:56:47 -0500

    We pass to slick2D :) -Almost all is deletead -slick 2D is added

---
+++ SpaceColony/src/com/spacecolony/game/Game.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 1448607
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.spacecolony.game;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author 1448607
+ */
+public class Game extends BasicGame{
+
+    public static final int SCALE = 0;
+    
+    private Image tm;
+    
+    public Game(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        tm = new Image("Images/Test.png");
+        tm.setFilter(Image.FILTER_NEAREST);
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        g.scale(10, 10);
+        g.drawImage(tm, 0, 0);
+    }
+    
+}
--- SpaceColony/src/com/spacecolony/game/Init.java
+++ SpaceColony/src/com/spacecolony/game/Init.java
@@ -1,15 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 1448607
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.spacecolony.game;
 
-import com.spacecolony.game.graphics.Frame;
-import javafx.application.Application;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
 
 /**
- * Initialyses the game
  *
  * @author 1448607
  */
@@ -19,7 +29,15 @@ public class Init {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Application.launch(Frame.class, args);
+        
+        
+        try {
+            AppGameContainer app = new AppGameContainer(new Game("test"));
+            app.setDisplayMode(500, 400, false);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 
 }
