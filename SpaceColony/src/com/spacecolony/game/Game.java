8eeaeab375d8b8629e172aea99006b1701f7955b
1448607 <1448607@li-h023-0t02>
2015-12-11 11:02:21 -0500

    We pass to slick2D :) -Almost all is deletead -slick 2D is added

--- SpaceColony/src/com/spacecolony/game/Game.java
+++ SpaceColony/src/com/spacecolony/game/Game.java
@@ -21,6 +21,7 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 
 /**
  *
